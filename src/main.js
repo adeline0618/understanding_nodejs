b373405648eebd4ce4290ebf600e321588abc4ed
Swan <adeliine.0618@gmail.com>
2022-12-11 23:55:45 +0900

    Feat : 프레임워크 없이 간단한 API서버

--- src/main.js
+++ src/main.js
@@ -1,13 +1,6 @@
 // @ts-check
 // Formatting(Prettier), Linting(ESLint), Type Checking(TypeScript)
 
-// 프레임워크 없이 간단한 토이프로젝트 웹 서버 만들어보기
-/**
- * 블로그 포스팅 서비스
- * 로컬 파일을 데이터베이스로 활용(JSON)
- * 인증 로직은 넣지 않는다.
- * RESTful API를 사용
- */
 const http = require('http')
 
 /**
@@ -69,7 +62,6 @@ const server = http.createServer((req, res) => {
        * @property {string} title
        * @property {string} content
        */
-
       /** @type {CreatePostBody} */
       const body = JSON.parse(data)
       posts.push({
